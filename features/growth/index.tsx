6857c4909240fe2a0732c064479b81c49721aee3
Fukurou3 <fukurou@gmail.com>
2025-06-08 00:27:36 +0900

    成長画面骨組み完了

--- features/growth/index.tsx
+++ features/growth/index.tsx
@@ -1,6 +1,5 @@
 //C:\Users\fukur\task-app\app\app\(tabs)\growth\index.tsx
-import GrowthScreen from '@/features/growth';
-
+import GrowthScreen from '@/features/growth/GrowthScreen';
 export default function GrowthTab() {
   return <GrowthScreen />;
 }
