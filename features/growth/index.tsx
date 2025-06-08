08e91fa15dc1bcccda17ed37a64396d218f37168
Fukurou3 <fukurou@gmail.com>
2025-06-08 15:48:59 +0900

    成長画面リセット

--- features/growth/index.tsx
+++
@@ -1,5 +0,0 @@
-//C:\Users\fukur\task-app\app\app\(tabs)\growth\index.tsx
-import GrowthScreen from '@/features/growth/GrowthScreen';
-export default function GrowthTab() {
-  return <GrowthScreen />;
-}
